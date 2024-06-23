bde0c7ad3338c86c2745e5270de623cce5dd7db6
Great-Stone <hahohh@gmail.com>
2024-06-23 11:58:29 +0900

    add 8080

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "catapp_url" {
-  value = [for eip in aws_eip.hashicat: "http://${eip.public_dns}"]
+  value = [for eip in aws_eip.hashicat : "http://${eip.public_dns}"]
 }
 
 output "catapp_ip" {
-  value = [for eip in aws_eip.hashicat: "http://${eip.public_ip}"]
+  value = [for eip in aws_eip.hashicat : "http://${eip.public_ip}"]
 }
